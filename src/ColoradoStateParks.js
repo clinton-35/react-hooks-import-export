0eec666ccb6ee60a8269c32702ddf3a2f8a3c9e4
clinton <clinton.wambua@student.moringaschool.com>
2023-04-19 09:47:03 +0300

    complete

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,13 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import {trees, wildlife, elevation} from './parks/RockyMountain'
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+console.log (trees)
+wildlife()
+elevation()
+export default ColoradoStateParks;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+ReactDOM.render(<ColoradoStateParks/> , document.getElementById("root"));
